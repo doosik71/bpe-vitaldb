6f410a0050024ee2869713ba17690f753cbd5b21
Doosik Kim <doosik@etri.re.kr>
2026-06-12 08:44:36 +0900

    Move underperforming models to __excluded_models
    - relocate conv_reg_nas, duo, pulse_resnet1d, pulsew_resnet1d,
      pulsewo_resnet1d, and pulsewoq_resnet1d out of the active models directory
    - remove their imports and __all__ entries from __init__.py; leave
      commented-out lines as a reference breadcrumb

--- bpe/models/__init__.py
+++ bpe/models/__init__.py
@@ -6,27 +6,29 @@ from bpe.models.cnn_bilstm_at import CNNBiLSTMAttention
 from bpe.models.conv_reg import ConvReg
 from bpe.models.conv_reg_at import ConvRegAt
 from bpe.models.conv_reg_ds import ConvRegDs
-from bpe.models.conv_reg_nas import ConvRegNas
 from bpe.models.minception import MInception
 from bpe.models.mtae import MTAE
 from bpe.models.mtae_tr import MTAE_TR
 from bpe.models.naive import NaiveConstant
-from bpe.models.pulse_resnet1d import PulseResNet1D
-from bpe.models.pulsew_resnet1d import PulseWResNet1D
-from bpe.models.pulsewo_resnet1d import PulseWOResNet1D
-from bpe.models.pulsewoq_resnet1d import PulseWOQResNet1D
-from bpe.models.registry import (
-    create_model,
-    get_model_class,
-    list_models,
-    register_model,
-)
 from bpe.models.resnet1d import ResNet1D
 from bpe.models.resnet1d_micro import ResNet1DMicro
 from bpe.models.resnet1d_mini import ResNet1DMini
 from bpe.models.resnet1d_tiny import ResNet1DTiny
 from bpe.models.st_resnet import SpectroTemporalResNet
 from bpe.models.xresnet1d import XResNet1D101
+
+from bpe.models.registry import (
+    create_model,
+    get_model_class,
+    list_models,
+    register_model,
+)
+
+# from bpe.models.conv_reg_nas import ConvRegNas
+# from bpe.models.pulse_resnet1d import PulseResNet1D
+# from bpe.models.pulsew_resnet1d import PulseWResNet1D
+# from bpe.models.pulsewo_resnet1d import PulseWOResNet1D
+# from bpe.models.pulsewoq_resnet1d import PulseWOQResNet1D
 
 __all__ = [
     "ACFA",
@@ -35,15 +37,15 @@ __all__ = [
     "ConvReg",
     "ConvRegAt",
     "ConvRegDs",
-    "ConvRegNas",
+    # "ConvRegNas",
     "MInception",
     "MTAE",
     "MTAE_TR",
     "NaiveConstant",
-    "PulseResNet1D",
-    "PulseWResNet1D",
-    "PulseWOResNet1D",
-    "PulseWOQResNet1D",
+    # "PulseResNet1D",
+    # "PulseWResNet1D",
+    # "PulseWOResNet1D",
+    # "PulseWOQResNet1D",
     "ResNet1D",
     "ResNet1DMicro",
     "ResNet1DMini",
